66dba6136a21b9efaf6ac8ed7f3b62c4981188d9
bengHak <qkwl4678@naver.com>
2021-09-04 15:00:59 +0900

    🎉 initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WebtoonShorts' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WebtoonShorts
+
+end
---
+++ WebtoonShorts/Scenes/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  WebtoonShorts
+//
+//  Created by byunghak on 2021/09/04.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
